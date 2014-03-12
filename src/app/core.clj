8afc156bc21f0cd86548539f56370f2e6275d4b6
Alexandr Notchenko <avnotchenko@gmail.com>
2014-03-12 15:24:56 +0400

    removed useless func

--- src/app/core.clj
+++ src/app/core.clj
@@ -14,15 +14,6 @@
 	(getGameJson "data/wwf_test_game.json"))
 
 
-(defn assemble-nodes
-     [tree prefix]
-     (flatten (map (fn [t]
-         (if (empty? (tree t))
-             (apply str (concat prefix t))
-             (assemble-nodes (tree t) (concat prefix t))))
-         (keys tree))))
-
-
 (defn -main []
 	(def D_trie (reduce radix/insert {} 
 		(radix/read_words_from_file "data/full_word_list.csv")))
